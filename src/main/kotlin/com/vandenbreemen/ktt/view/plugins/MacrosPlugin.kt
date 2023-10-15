d718ee807e45cc4695a00522463ecfc05913942d
kevin <kevin.vandenbreemen@gmail.com>
2023-10-14 22:03:23 -0700

    Code cleanup

--- src/main/kotlin/com/vandenbreemen/ktt/view/plugins/MacrosPlugin.kt
+++ src/main/kotlin/com/vandenbreemen/ktt/view/plugins/MacrosPlugin.kt
@@ -9,31 +9,24 @@ class MacrosPlugin(private val macroRegistry: MacroRegistry): PageRenderingPlugi
 
         var resultantMarkdown = markdown
 
-        println("MATCHES\n=======================")
         allMacros.forEach { macro->
             val macroExpression = macro.value
             val macroName = macro.groupValues[1]
-            val argumentsRaw = macro.groupValues[2].split("[\\s]*[,][\\s]*")
-            println("args=$argumentsRaw")
-
 
             val argMap: MutableMap<String, String> = mutableMapOf()
-            if(argumentsRaw.isNotEmpty()) {
-                val argPairs: List<Pair<String, String>> = argumentsRaw.mapNotNull { argument ->
+            if(macro.groupValues[2].isNotEmpty()) {
+                val argumentsRaw = macro.groupValues[2].split("[\\s]*[,][\\s]*")
+                argumentsRaw.mapNotNull { argument ->
                     argument.split("=").let { rawPair ->
-                        println("rawPair=${rawPair}")
                         if(rawPair.size != 2) {
                             return@mapNotNull null
                         }
                         Pair(rawPair[0], rawPair[1])
                     }
-                }.toList()
-                argPairs.forEach { pair ->
+                }.toList().forEach { pair->
                     argMap[pair.first] = pair.second
                 }
             }
-
-            println("expression=$macroExpression, macroName=$macroName\nvalues=${macro.groupValues}")
 
             macroRegistry.getMacroByName(macroName) ?.let { macro ->
                 resultantMarkdown = resultantMarkdown.replace(macroExpression, macro.execute(argMap))
