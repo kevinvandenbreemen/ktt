f311c077f23e7f2856f2da71db9ca3a74021d9a3
kevin <kevin.vandenbreemen@gmail.com>
2023-10-17 16:32:17 -0700

    Github style markdown

--- src/main/kotlin/com/vandenbreemen/ktt/interactor/MarkdownInteractor.kt
+++ src/main/kotlin/com/vandenbreemen/ktt/interactor/MarkdownInteractor.kt
@@ -1,7 +1,7 @@
 package com.vandenbreemen.ktt.interactor
 
 import com.vandenbreemen.ktt.util.normalizeToLineFeed
-import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
+import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
 import org.intellij.markdown.html.HtmlGenerator
 import org.intellij.markdown.parser.MarkdownParser
 
@@ -11,7 +11,7 @@ class MarkdownInteractor {
 
         val toTranslate = markdown.normalizeToLineFeed()
 
-        val flavour = CommonMarkFlavourDescriptor()
+        val flavour = GFMFlavourDescriptor()
         val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(toTranslate)
         return HtmlGenerator(toTranslate, parsedTree, flavour).generateHtml()
     }
