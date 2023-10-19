c59f58a65224e81da7e28ec5694799f02a9b9ca9
kevin <kevin.vandenbreemen@gmail.com>
2023-10-18 17:25:12 -0700

    Make the breadcrumbs section customizable

--- src/main/kotlin/com/vandenbreemen/ktt/model/StylesheetType.kt
+++ src/main/kotlin/com/vandenbreemen/ktt/model/StylesheetType.kt
@@ -1,9 +1,11 @@
 package com.vandenbreemen.ktt.model
 
+import com.vandenbreemen.ktt.web.Classes.breadcrumbs
 import com.vandenbreemen.ktt.web.Classes.controlPanel
 import com.vandenbreemen.ktt.web.Classes.wikiEntry
 
 enum class StylesheetType(val cssClass: String) {
     WikiEntry(wikiEntry),
-    ControlPanel(controlPanel)
+    ControlPanel(controlPanel),
+    BreadcrumbsSection(breadcrumbs),
 }
