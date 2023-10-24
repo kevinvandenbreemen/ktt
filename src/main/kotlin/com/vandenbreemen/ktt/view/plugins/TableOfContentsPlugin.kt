0895eae9257f94d988df07f78d6d8f5f56bf673a
kevin <kevin.vandenbreemen@gmail.com>
2023-10-24 15:35:43 -0700

    Start work on a plugin to generate table of contents

---
+++ src/main/kotlin/com/vandenbreemen/ktt/view/plugins/TableOfContentsPlugin.kt
@@ -0,0 +1,40 @@
+package com.vandenbreemen.ktt.view.plugins
+
+import com.vandenbreemen.ktt.view.PageRenderingPlugin
+
+class TableOfContentsPlugin: PageRenderingPlugin {
+
+    override fun process(markdownContent: String): String {
+        val headersRegex = Regex("(?m)^#+\\s+(.*)$")
+
+        val headers = headersRegex.findAll(markdownContent)
+            .map {
+                println("gr0=${it.groupValues[0]}")
+                println("gr1=${it.groupValues[1]}")
+                it.groupValues[0] to it.groupValues[1] }
+            .toList()
+
+        val headerLinks = mutableMapOf<String, String>()
+
+        val tableOfContents = StringBuilder()
+
+        if (headers.isNotEmpty()) {
+            tableOfContents.append("Table of Contents\n")
+            for ((hashes, header) in headers) {
+                println("Processing '$header'")
+                val indentation = "  ".repeat(hashes.count { it == '#' } - 1)
+                tableOfContents.append("$indentation- [${header.trim()}](#${header.toLowerCase().replace("\\s+".toRegex(), "-").also {link-> 
+                    headerLinks[hashes] = link
+                }})\n")
+            }
+        }
+
+        println(headerLinks)
+        var updatedMarkdown = markdownContent
+        headerLinks.entries.forEach { headerToLink->
+            updatedMarkdown = updatedMarkdown.replace(headerToLink.key, "<section id=\"${headerToLink.value}\">\n\n${headerToLink.key}\n")
+        }
+
+        return StringBuilder(tableOfContents.toString()).append("\n\n").append(updatedMarkdown).toString()
+    }
+}
